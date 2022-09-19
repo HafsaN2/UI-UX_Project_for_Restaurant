35e356dc95eee1738bd95b581fed707e9c53166d
HAFSAnCR7 <52420901+HAFSAnCR7@users.noreply.github.com>
2022-09-19 18:10:36 -0400

    Create data.js

---
+++ src/constants/data.js
@@ -0,0 +1,104 @@
+mport images from './images';
+
+const drinks = [
+  {
+    title: 'Mango Lassi',
+    price: '$3',
+  },
+  {
+    title: 'Sweet Lassi',
+    price: '$3',
+  },
+  {
+    title: 'Kashmiri Chai',
+    price: '$3',
+  },
+  {
+    title: 'Almond Pistachio Milk',
+    price: '$3',
+  },
+  {
+    title: 'Chaas',
+    price: '$3',
+  },
+  {
+    title: 'Rooh Afza',
+    price: '$2',
+  },
+  {
+    title: 'Sugarcane Juice',
+    price: '$2',
+  },
+  {
+    title: 'Sharbat',
+    price: '$2',
+  },
+  {
+    title: 'Shikanji',
+    price: '$2',
+  },
+];
+
+const desserts = [
+  {
+    title: 'Falooda',
+    price: '$5',
+  },
+  {
+    title: "Rabri",
+    price: '$5',
+  },
+  {
+    title: 'Gajar Ka Halwa',
+    price: '$5',
+  },
+  {
+    title: 'Kheer',
+    price: '$5',
+  },
+  {
+    title: 'Jalebi',
+    price: '$2',
+  },
+  {
+    title: 'Gulab Jamun',
+    price: '$3',
+  },
+  {
+    title: 'Kulfi',
+    price: '$2',
+  },
+  {
+    title: 'Lab E Shireen',
+    price: '$5',
+  },
+  {
+    title: 'Kaju Katli',
+    price: '$4',
+  },
+];
+
+const awards = [
+  {
+    imgUrl: images.award02,
+    title: 'Bib Gourmond',
+    subtitle: 'Friendly establishment that serves good food at moderate prices',
+  },
+  {
+    imgUrl: images.award01,
+    title: 'Rising Star',
+    subtitle: 'Co-chefs Firdous and Nasreen are phenomenal',
+  },
+  {
+    imgUrl: images.award05,
+    title: 'AA Hospitality',
+    subtitle: 'Each staff member makes you feel like family',
+  },
+  {
+    imgUrl: images.award03,
+    title: 'Outstanding Chef',
+    subtitle: 'The best Pakistani chef in New York',
+  },
+];
+
+export default { drinks, desserts, awards };
